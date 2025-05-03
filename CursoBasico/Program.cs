bc28b53ab67e3828bf68eb823befcd45675df0fe
jose villegas <josevillegas13600@gmail.com>
2025-05-03 14:27:42 -0400

    arreglos

--- CursoBasico/Program.cs
+++ CursoBasico/Program.cs
@@ -16,7 +16,7 @@ do
 for (int cont = 0; cont <5; cont++)
 {
     Console.WriteLine(cont);
-}*/
+}
 
 //lectura de datos
 string nombre = "";
@@ -32,4 +32,16 @@ string textonum2 = "";
 textonum2 = Console.ReadLine();
 num2 = Convert.ToInt32(textonum2);
 int resultado_suma = num1 + num2;
-Console.WriteLine("El resultado de la suma es: "+resultado_suma);
+Console.WriteLine("El resultado de la suma es: "+resultado_suma);*/
+
+//areglos
+int[] arreglo1 = new int[4];
+Console.WriteLine(arreglo1.Length);
+arreglo1[0] = 12;
+arreglo1[1] = 23;
+arreglo1[2] = 84;
+arreglo1[3] = 40;
+for (int cont = 0; cont < arreglo1.Length; cont++)
+{
+    Console.WriteLine(arreglo1[cont]);
+}
