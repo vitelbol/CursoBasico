830b246430c11451f94d8ceb0af14459bcf7d006
jose villegas <josevillegas13600@gmail.com>
2025-05-03 12:38:28 -0400

    Agregar archivos de proyecto.

---
+++ CursoBasico/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int cajaentero = 15;
+int cajaenteronegativo = -4;
+int cajaentero2 = 1000;
+double materia_Mate = 4.5;
+double materia_espaniol = 8;
+decimal dinero = (decimal) 250.7652315; //valor decimal
+decimal dineroMio = 630.12354872M; //valor decimal
+char letra = 'd';
+string Texto_5 = "Hola soy jose";
+bool verdadero = true;
+bool falso = false;
+Console.WriteLine(cajaentero);
+Console.WriteLine(cajaenteronegativo);
+Console.WriteLine(cajaentero2);
+Console.WriteLine(materia_Mate);
+Console.WriteLine(materia_espaniol);
+Console.WriteLine(dinero);
+Console.WriteLine(dineroMio);
+Console.WriteLine(letra);
+Console.WriteLine(Texto_5);
+Console.WriteLine(verdadero);
+Console.WriteLine(falso);
