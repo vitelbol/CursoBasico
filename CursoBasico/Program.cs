fbbaec5e4ad603b37e8d52dfbd6396d47b00be76
jose villegas <josevillegas13600@gmail.com>
2025-05-03 14:19:47 -0400

    ingreso de datos

--- CursoBasico/Program.cs
+++ CursoBasico/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numero = 1;
-/*
+/*int numero = 1;
+
 while(numero <5)
 {
     //El codigo que este aqui se va a ejecturar mientras la condicion del ciclo se cumpla
@@ -12,8 +12,24 @@ do
 {
     numero+=1;
     Console.WriteLine(numero);
-} while (numero < 3);*/
+} while (numero < 3);
 for (int cont = 0; cont <5; cont++)
 {
     Console.WriteLine(cont);
-}
+}*/
+
+//lectura de datos
+string nombre = "";
+Console.WriteLine("Por favor inserta tu nombre");
+nombre = Console.ReadLine();
+Console.WriteLine("\n Tu nombre es: " + nombre);
+Console.WriteLine("\nIngresa el primer numero: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\n El numero es: " + num1);
+Console.WriteLine("\nIngresa el segundo numero: ");
+int num2 = 0;
+string textonum2 = "";
+textonum2 = Console.ReadLine();
+num2 = Convert.ToInt32(textonum2);
+int resultado_suma = num1 + num2;
+Console.WriteLine("El resultado de la suma es: "+resultado_suma);
